f335c37eb105ee8685dc8f9d479467c4be90d5a6
Roman Pervuhin <rg.pervuhin5198@gmail.com>
2021-09-03 16:51:02 +0500

    Update BinarySearch.cs

--- BinarySearch/C#/BinarySearch.cs
+++ BinarySearch/C#/BinarySearch.cs
@@ -3,35 +3,7 @@ using System;
 namespace BinarySearch
 {
     class Program
-    {
-        public void BinSearch(int[] arr, int N, int number)
-        {
-            int low = 0;
-            int high = N;
-
-            while (low < high - 1) {
-                int mid = (low + high) / 2;
-
-                if (number < arr[mid])
-                {
-                    high = mid;
-                }
-                else
-                {
-                    low = mid;
-                }
-            }
-
-            if(arr[low] == number)
-            {
-                Console.WriteLine("arr[{0}]={1}", low, number);
-            }
-            else
-            {
-                Console.WriteLine("Not found :(");
-            }
-        }
-
+    {        
         static void Main(string[] args)
         {
             int N = Convert.ToInt32(Console.ReadLine());
